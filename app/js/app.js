d3cc17b43bf45939049786a5ccab44bc8fc45caf
Davyd Tevzadze <pawtod22@gmail.com>
2023-04-09 13:54:56 +0300

    errors fix

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,14 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 import { splitingText } from './assets/splitingText'
 
-gsap.to(window, {duration: .1, scrollTo: {y: 0}, onComplete: () => {
-	document.body.classList.add('unscroll')
-	document.body.classList.remove('hiddenlines')}
-});
+
 
 window.addEventListener('load', () => {
+	gsap.to(window, {duration: .1, scrollTo: {y: 0}, onComplete: () => {
+		document.body.classList.add('unscroll')
+		document.body.classList.remove('hiddenlines')
+		sectionsFunction()}
+	});
 
 	let backgroundVideo = (window.innerWidth > 515) ? document.querySelector('.background-video.desctop') : document.querySelector('.background-video.mobile')
 
@@ -21,83 +23,93 @@ window.addEventListener('load', () => {
 	}, 2000);
 	
 	function backgroundVideoFunction() {
-		backgroundVideo.play()
-		backgroundVideo.pause()
 		backgroundVideo.currentTime = 0
 		ScrollTrigger.create({
 			trigger: document.body,
 			start: 'top top',
 			end: 'bottom bottom',
 			onUpdate: self => {
-				backgroundVideo.pause()
-				backgroundVideo.currentTime = backgroundVideo.duration * self.progress * 0.5
+				backgroundVideo.currentTime = backgroundVideo.duration * self.progress
 			}
 		})
 	}
 
-	let videoSection = document.querySelectorAll('.video-section') 
+	function sectionsFunction() {
+		let videoSection = document.querySelectorAll('.video-section') 
 
-	videoSection.forEach( section => {
-		let video = section.querySelector('video')
-		let content = section.querySelector('.content')
-		let textElements =  section.querySelectorAll('.split-line.child')
-		video.play()
-		video.pause()
-		video.currentTime = 0
-		gsap.set([video, textElements], {
-			opacity: 0,
-		})
-		
-		ScrollTrigger.create({
-			trigger: section,
-			pin: content,
-			start: 'center center',
-			end: 'bottom center',
-			pinSpacer: false,
-			onUpdate: self => {
-				video.pause()
-				video.currentTime = video.duration * self.progress
-			},
-			onEnter: () => {
-				gsap.to(video, {
-					opacity: 1,
-					duration: .3
-				})
-				gsap.to(textElements, {
-					y: 0,
-					opacity: 1,
-					stagger: .08,
-					delay: .2
-				})
-			},
-			onLeave: () => {
-				gsap.to(textElements, {
-					y: '100%',
+		videoSection.forEach( section => {
+			let video = section.querySelector('video')
+			let content = section.querySelector('.content')
+			let textElements =  section.querySelectorAll('.split-line.child')
+			if(video) {
+				video.currentTime = 0
+				gsap.set(video, {
 					opacity: 0,
 				})
-				video.pause()
-			},
-			onEnterBack: () => {
-				gsap.to(textElements, {
-					y: 0,
-					opacity: 1,
-					stagger: .08,
-					delay: .2
-				})
-			},
-			onLeaveBack: () => {
-				gsap.to(video, {
-					opacity: 0,
-					duration: .3
-				})
-				gsap.to(textElements, {
-					y: '100%',
-					opacity: 0,
-				})
-				video.pause()
 			}
+			
+			gsap.set( textElements, {
+				opacity: 0,
+			})
+			
+			ScrollTrigger.create({
+				trigger: section,
+				pin: content,
+				start: 'center center',
+				end: 'bottom center',
+				pinSpacer: false,
+				onUpdate: self => {
+					if(video) {
+						video.currentTime = video.duration * self.progress
+					}
+				},
+				onEnter: () => {
+					if(video) {
+						gsap.to(video, {
+							opacity: 1,
+							duration: .3
+						})
+					}
+					
+					gsap.to(textElements, {
+						y: 0,
+						opacity: 1,
+						stagger: .08,
+						delay: .2
+					})
+				},
+				onLeave: () => {
+					gsap.to(textElements, {
+						y: '100%',
+						opacity: 0,
+					})					
+				},
+				onEnterBack: () => {
+					gsap.to(textElements, {
+						y: 0,
+						opacity: 1,
+						stagger: .08,
+						delay: .2
+					})
+				},
+				onLeaveBack: () => {
+					if(video) {
+						gsap.to(video, {
+							opacity: 0,
+							duration: .3
+						})
+					}
+					
+					gsap.to(textElements, {
+						y: '100%',
+						opacity: 0,
+					})
+					
+				}
+			})
 		})
-	})
+	}
+	
 
 	
 	function prelodaderHide() {
