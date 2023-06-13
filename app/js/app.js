767a237c92ba78ed1ab8a789483488a9fdd74a92
Kirill Trachuk <>
2023-06-13 20:52:25 +0300

    add new pages

--- app/js/app.js
+++ app/js/app.js
@@ -300,20 +300,69 @@ window.addEventListener('load', () => {
 			
 		}
 	}
-	const numberElement = document.querySelector('.number');
-	const plusButton = document.querySelector('.plus');
+
+	const addButton = document.querySelector('.plus');
 	const minusButton = document.querySelector('.minus');
+	const fromInput = document.querySelector('.from-input');
+	const numberElement = document.querySelector('.number');
+	let total = document.querySelector('.total span')
+	let number = 1;
+ 	///price
+	total.innerHTML = 0.2 * number;
 
-	let number = 0;
-
-	plusButton.addEventListener('click', () => {
+	addButton.addEventListener('click', () => {
+		const newInput = document.createElement('input');
+		
+		newInput.type = 'text';
+		newInput.placeholder = 'enter your wallet address';
+		fromInput.appendChild(newInput);
 		number++;
 		numberElement.textContent = number;
+		total.innerHTML = (0.2 * number).toFixed(1);
 	});
 
 	minusButton.addEventListener('click', () => {
-		number--;
-		numberElement.textContent = number;
+		const lastInput = fromInput.lastElementChild;
+		if (lastInput) {
+			fromInput.removeChild(lastInput);
+			number--;
+			numberElement.textContent = number;
+			total.innerHTML = (0.2 * number).toFixed(1);
+		}
+		
 	});
-	
+
+	const tabComp = document.querySelector('.composition')
+	const tabRewards = document.querySelector('.rewards')
+	const tabs = document.querySelector('.tabs')
+	const containerInside = document.querySelector('.tabs-container')
+	tabRewards.addEventListener('click', () => {
+		tabs.classList.add('active')
+		containerInside.classList.add('active')
+	})
+	tabComp.addEventListener('click', () => {
+		tabs.classList.remove('active')
+		containerInside.classList.remove('active')
+	})
+
+	const addButtonPub = document.querySelector('.plus-public');
+	const minusButtonPub = document.querySelector('.minus-public');
+	const numberElementPub = document.querySelector('.number-public');
+	let totalPub = document.querySelector('.total-public span')
+	let numberPub = 1;
+	totalPub.innerHTML = 0.2 * numberPub;
+
+	addButtonPub.addEventListener('click', () => {
+		numberPub++;
+		numberElementPub.textContent = numberPub;
+		totalPub.innerHTML = (0.2 * numberPub).toFixed(1);
+	});
+
+
+	minusButtonPub.addEventListener('click', () => {
+		numberPub--;
+		numberElementPub.textContent = numberPub;
+		totalPub.innerHTML = (0.2 * numberPub).toFixed(1);
+	})
+		
 })
